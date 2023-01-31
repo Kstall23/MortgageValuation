0a8aed24592a7799e3c08942b75f9c13ff799d2b
jjbrown23 <71121776+jjbrown23@users.noreply.github.com>
2023-01-31 12:17:11 -0700

    tried reading/writing data

--- backend/machineLearning/model.py
+++ backend/machineLearning/model.py
@@ -1,1 +1,15 @@
+import numpy as np
+import pandas as pd
+
 print("Hey look, maybe someday this will provide a prediction point.")
+
+# read in training data file as a pandas dataframe, assigning appropriate headers
+data = pd.read_csv('backend\database\TrainingData.csv', sep=',', names=["Title", "Owner", "Asset Type", "Unpaid Principal Balance", "Loan Term", "Note Rate", "PPP", "Value", "LTV", "PropType", "Occupancy"])
+
+for index, row in data.iterrows():
+
+    print("Row "+ str(index))
+    for val in row:
+        print(val)
+    print()
+    print()
