3c3c71e0df0eace72155211f428ace35cc8496e7
Lauren Helbling <laurengraceh97@gmail.com>
2023-03-08 16:59:47 -0700

    delete file function

--- frontend/index.py
+++ frontend/index.py
@@ -8,11 +8,23 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 app.secret_key = "super secret key"
 TEST_API_URL = "https://api.coindesk.com/v1/bpi/currentprice.json"
+FILES_PATH = '../backend/database/individualFiles'
+
+
+def get_filenames():
+    files = os.listdir(FILES_PATH)
+    return files
+
+
+@app.route('/delete/<file_name>')
+def delete_file(file_name):
+    os.remove(FILES_PATH + "/" + file_name)
+    return redirect('/')
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', filenames=get_filenames())
 
 
 @app.route("/", methods=['POST'])
@@ -28,13 +40,19 @@ def uploadFiles():
 
 
 @app.route('/onImport/<file_name>')
-def mortageDetails(file_name):
+def mortgageDetails(file_name):
     file_path = "../backend/database/individualFiles/" + file_name
     df = pd.read_csv(file_path)
     data = df.values[0]
     price_response = urllib.request.urlopen(TEST_API_URL, cafile=certifi.where()).read()
-    ppp = json.loads(price_response)['bpi']['USD']['rate']
-    return render_template('mortgageDetails.html', data=data, ppp=ppp)
+    bitcoinValueString = json.loads(price_response)['bpi']['USD']['rate']
+    bitcoinValue = int(re.sub(r'[^\w\s]', '', bitcoinValueString))
+    if bitcoinValue > 23000:
+        bs = True
+    else:
+        bs = False
+    icon = "icon"
+    return render_template('mortgageDetails.html', data=data, recommendation=bs, icon=icon)
 
 
 if __name__ == '__main__':
