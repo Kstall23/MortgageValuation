0b8ca13b13e0daa279c9d6ad9171dd6c77dc24a4
jjbrown23 <71121776+jjbrown23@users.noreply.github.com>
2023-03-26 17:20:37 -0600

    more fun with data exploration

--- backend/machineLearning/ExploratoryAnalysis.py
+++ backend/machineLearning/ExploratoryAnalysis.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from datetime import datetime
 import git
 from git import Repo
 import os
-
+pd.set_option('display.max_columns', 11)
+pd.set_option('display.width', 170)
 
 
 os.chdir('..')
@@ -16,13 +19,38 @@ data = pd.read_csv(os.path.join(repo_dir, input_file_name), sep=',', names=['Yea
 # Summary Statistics
 print(data.describe())
 nines = data[data['HousingExpenseToIncome'] == 999]
+non = data[data['HousingExpenseToIncome'] != 999]
+
 print(nines)
+print(non.describe())
 print(len(nines) / len(data))
+
+''' Stuff for the change between UPBatAcquisition and OriginationValue '''
+# change  = data[data['UPBatAcquisition'] != data['OriginationValue']]
+# diff = []
+# for index, row in change.iterrows():
+#     diff.append(row['OriginationValue'] - row['UPBatAcquisition'])
+# Sdiff = pd.Series(diff)
+# print(Sdiff)
+# print(Sdiff.describe())
+# print(len(Sdiff[Sdiff < 0]))
+# print(change)
+# print(change.describe())
+# print(len(change) / len(data))
 
 print()
 print()
 
 # Correlation Matrix
-# corr = data.corr()
-# print(corr)
-# print()
+corr = data.corr()
+print(corr)
+print()
+# Heatmap for Correlation Matrix
+# mask = np.triu(np.ones_like(corr, dtype=bool)) # mask for the upper triangle
+# fig, ax = plt.subplots()
+# cmap = sns.diverging_palette(230,20, as_cmap=True)  # create color map
+# sns.heatmap(corr, mask=mask, cmap=cmap, center=0, linewidths=.75, cbar_kws={"shrink":.8})   # draw heatmap
+# plt.show()
+
+sns.pairplot(data)
+plt.savefig('pairplot.png')
