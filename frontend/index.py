b30f32cfdfc783b32888dac463a14ed99750a733
Lauren Helbling <laurengraceh97@gmail.com>
2023-04-13 11:45:59 -0600

    Add flag status

--- frontend/index.py
+++ frontend/index.py
@@ -12,16 +12,19 @@ app.config["DEBUG"] = True
 app.secret_key = "super secret key"
 FILES_PATH = '../backend/database/individualFiles'
 
+
 # Home Page
 @app.route('/')
 def index():
     return render_template('index.html', filenames=get_filenames())
+
 
 # Return File History
 @app.get("/")
 def get_filenames():
     files = os.listdir(FILES_PATH)
     return files
+
 
 # Upload Files
 @app.post("/")
@@ -34,44 +37,50 @@ def uploadFiles():
     else:
         flash("Please select a valid csv file before submitting.")
         return redirect('/')
-    return redirect('/'+uploaded_file.filename+'/load')
+    return redirect('/' + uploaded_file.filename + '/load')
 
-#Loading Simulation
+
+# Loading Simulation
 @app.get('/<file_name>/load')
 def loadMortgage(file_name):
     return render_template('loader.html', file_name=file_name)
 
-#Generate Predicted Value
+
+# Generate Predicted Value
 @app.get('/<file_name>/predict')
 def predict(file_name):
     file_path = "../backend/database/individualFiles/" + file_name
     df = pd.read_csv(file_path)
-    df['PPP'] = predictionModel.testFromUpload(file_name)
+    df['PPP'], df['delinq'], df['appr'], df['depr'] = predictionModel.testFromUpload(file_name)
     df.to_csv(file_path)
     return jsonify("Prediction Complete!")
 
-#View Specified Mortgage
-#TODO change frontend to match test file
+
+# View Specified Mortgage
 @app.get('/<file_name>')
 def mortgageDetails(file_name):
     file_path = "../backend/database/individualFiles/" + file_name
     df = pd.read_csv(file_path)
     data = df.to_dict('records')[0]
-    print(data)
     for item in ['Price', 'UPBatAcquisition', 'PPP', 'OriginationValue', 'PropertyValue', 'CurrentPropertyValue']:
         if not isinstance(data[item], str):
             data[item] = "${:,.2f}".format(data[item])
     for item in ['InterestRate', 'LTVRatio']:
         if not isinstance(data[item], str):
-            data[item] = "{:.2%}".format(data[item]/100)
-    ppp = "Recommended"
+            data[item] = "{:.2%}".format(data[item] / 100)
+    if data['delinq'] or data['depr']:
+        ppp = "Not Recommended"
+    else:
+        ppp = "Recommended"
     return render_template('mortgageDetails.html', data=data, recommendation=ppp, file_name=file_name)
+
 
 # Delete Specified File
 @app.route("/delete/<file_name>")
 def delete_file(file_name):
     os.remove(FILES_PATH + "/" + file_name)
     return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
