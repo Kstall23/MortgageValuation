0de1b27f33158ad2a3597e167bc00aa4f352d418
Lauren Helbling <laurengraceh97@gmail.com>
2023-04-25 09:52:30 -0600

    change additional info ouput

--- frontend/static/scripts/viewMortgage.js
+++ frontend/static/scripts/viewMortgage.js
@@ -1,19 +1,43 @@
 document.addEventListener("DOMContentLoaded", function(event) {
                 let questionIcon = document.getElementById("question");
                 let additionalInfo = document.getElementById("explore");
+                let delinq = document.getElementById('delinq')
+                let appr = document.getElementById('appr')
+                let depr = document.getElementById('depr')
+                let flag_values = [delinq, appr, depr]
                 questionIcon.onclick = function(){
                     additionalInfo.style.display="block";
                 }
                 let flag_lines = document.getElementsByClassName("minor_line");
                 for(let i=0; i<3; i++){
                     let checkIcon = flag_lines[i].previousElementSibling;
-                    if(flag_lines[i].textContent.includes("True")){
-                        checkIcon.src="/static/icons/check.png";
-                        flag_lines[i].textContent = "hello";
+                    if(i==0){
+                        if(flag_values[i].textContent.includes("True")){
+                            checkIcon.src="/static/icons/x-mark.png";
+                            flag_lines[i].textContent = "Warning: this mortgage has a history of delinquency.";
+                        }
+                        else{
+                            checkIcon.src="/static/icons/check.png";
+                            flag_lines[i].textContent = "This mortgage does not have a history of delinquency.";
+                        }
                     }
-                    else{
-                        checkIcon.src="/static/icons/x-mark.png";
-                        flag_lines[i].textContent = "hello";
+                    else if(i==1) {
+                        if (flag_values[i].textContent.includes("True")) {
+                            checkIcon.src = "/static/icons/check.png";
+                            flag_lines[i].textContent = "This mortgage is appreciating in value!";
+                        } else {
+                            checkIcon.src = "/static/icons/x-mark.png";
+                            flag_lines[i].textContent = "This mortgage is not appreciating in value.";
+                        }
+                    }
+                    else {
+                        if (flag_values[i].textContent.includes("True")) {
+                            checkIcon.src = "/static/icons/x-mark.png";
+                            flag_lines[i].textContent = "Warning: this mortgage is depreciating in value!";
+                        } else {
+                            checkIcon.src = "/static/icons/check.png";
+                            flag_lines[i].textContent = "This mortgage is not depreciating in value.";
+                        }
                     }
                 }
             });
